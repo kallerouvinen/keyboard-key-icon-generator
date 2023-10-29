8d1b42149d3b8de3cce88d81a5a3e2823901d617
kallerouvinen <kalle_rouvinen@hotmail.com>
2023-10-29 11:00:14 +0200

    Add mouse keys

--- key_names.py
+++ key_names.py
@@ -12,9 +12,11 @@ numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 function = ["F1", "F2", "F3", "F4", "F5", "F6", "F7",
             "F8", "F9", "F10", "F11", "F12"]
 
+mouse = ["LMB", "RMB", "MMB", "ScrlUp", "ScrlDown"]
+
 other = ["Enter", "Esc", "Backspace", "Tab", "Caps", "Ins",
          "Home", "PgUp", "Del", "End", "PgDown", "Ctrl",
          "Shift", "Alt", "RCtrl", "RShift", "RAlt", "Space"]
 
 # Change this to change the keys to generate
-keys = arrows + letters + numbers + function + other
+keys = arrows + letters + numbers + function + mouse + other
